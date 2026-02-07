d0526dc14d50e1949d147d397baaab7c868630ea
sandip-jadhav <98940858+sandipjadhav-tomtom@users.noreply.github.com>
2026-02-07 12:12:20 +0530

    #260 sangraha dataset download by lang filter

--- experiments/1_data_radar_and_acquisition/dataset_download/main.py
+++ experiments/1_data_radar_and_acquisition/dataset_download/main.py
@@ -1,18 +1,42 @@
 import boto3
 from huggingface_hub import hf_hub_download, list_repo_files
 import os
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(description="Download dataset from Hugging Face and upload to S3")
+    parser.add_argument('--category', choices=['verified', 'synthetic','unverified'], required=True, help='Choose the category: verified, synthetic, or unverified')
+    parser.add_argument('--lang', help='Specify the language if category is language (e.g., hindi)')
+    args = parser.parse_args()
+
     # Configuration
     repo_id = "ai4bharat/sangraha"
     bucket = "t1-dataacquisition-datasets"  # Replace with your S3 bucket name
     repo_type = "dataset"
 
+    # Determine prefix
+    if args.category == 'verified':
+        prefix = 'verified/'
+        if not args.lang:
+            parser.error("--lang is required when category is language")
+        else:
+            prefix = prefix+ args.lang + '/'
+    else:
+        parser.error("Invalid category")
+    
+    print ("Starting the download and upload process...")
+    print (f"Category: {args.category}, Language: {args.lang if args.lang else 'N/A'}, Prefix: {prefix}")
+
     # Initialize S3 client
     s3 = boto3.client('s3')
 
-    # Get list of files in the repo
-    files = list_repo_files(repo_id, repo_type=repo_type)
+    # Get list of files in the repo and filter by prefix
+    all_files = list_repo_files(repo_id, repo_type=repo_type)
+    files = [f for f in all_files if f.startswith(prefix)]
+
+    if not files:
+        print(f"No files found for prefix {prefix}")
+        return
 
     for filename in files:
         print(f"Downloading and uploading {filename}...")
